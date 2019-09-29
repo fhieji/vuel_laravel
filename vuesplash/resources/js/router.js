d61389931f3dfab4850d763ce932419f494ef989
fhieji <mtltmu@gmail.com>
2019-09-30 00:24:36 +0900

    implement errorhandling

--- vuesplash/resources/js/router.js
+++ vuesplash/resources/js/router.js
@@ -1,17 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-// ページコンポーネントをインポートする
 import PhotoList from './pages/PhotoList.vue'
 import Login from './pages/Login.vue'
+import SystemError from './pages/errors/System.vue'
 
-import store from './store' //追加
+import store from './store'
 
-// VueRouterプラグインを使用する
-// これによって<RouterView />コンポーネントなどを使うことができる
 Vue.use(VueRouter)
 
-// パスとコンポーネントのマッピング
 const routes = [
   {
     path: '/',
@@ -20,22 +17,23 @@ const routes = [
   {
     path: '/login',
     component: Login,
-    beforeEnter (to, from, next){
+    beforeEnter (to, from, next) {
       if (store.getters['auth/check']) {
         next('/')
       } else {
         next()
       }
     }
+  },
+  {
+    path: '/500',
+    component: SystemError
   }
 ]
 
-// VueRouterインスタンスを作成する
 const router = new VueRouter({
-  mode: 'history', // ★ 追加
+  mode: 'history',
   routes
 })
 
-// VueRouterインスタンスをエクスポートする
-// app.jsでインポートするため
 export default router
