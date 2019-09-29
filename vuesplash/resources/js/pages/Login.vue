2a134388d95574cc21941259f0d94a5398b616b9
fhieji <mtltmu@gmail.com>
2019-09-29 17:18:59 +0900

    routing

--- vuesplash/resources/js/pages/Login.vue
+++ vuesplash/resources/js/pages/Login.vue
@@ -59,13 +59,16 @@ export default {
     }
   },
   methods: {
-    login () {
-      console.log(this.loginForm)
+    async login () {
+      // authストアのloginアクションを呼び出す
+      await this.$store.dispatch('auth/login', this.loginForm)
+      // トップページに移動する
+      this.$router.push('/')
     },
     async register () {
-      //authストアのregisterアクションを呼び出す
-      await this.$store.disapatch('auth/reguster', this.registerForm)
-      //トップページに移動する
+      // authストアのresigterアクションを呼び出す
+      await this.$store.dispatch('auth/register', this.registerForm)
+      // トップページに移動する
       this.$router.push('/')
     }
   }
