60814e55e1aed882d2945adeff39305aa20974b6
fhieji <mtltmu@gmail.com>
2019-10-06 07:25:08 +0900

    upload photo

--- vuesplash/resources/js/store/index.js
+++ vuesplash/resources/js/store/index.js
@@ -2,12 +2,21 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import auth from './auth'
+import message from './message'
 
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
     modules: {
         auth
+    }
+})
+
+const store = new Vuex.Store({
+    modules: {
+        auth,
+        error,
+        message
     }
 })
 
